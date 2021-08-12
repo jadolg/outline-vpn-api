7f4f95eeac13dc2ba4f3e83b9b8b1f5893117e0b
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-08-12 17:15:51 +0200

    Disable linting for client redifined

--- test_outline_vpn.py
+++ test_outline_vpn.py
@@ -13,16 +13,16 @@ from outline_vpn import OutlineVPN
 def client() -> OutlineVPN:
     """This generates a client from the credentials provided in the environment"""
     assert os.getenv("OUTLINE_CREDENTIALS")
-    client = OutlineVPN(api_url=os.getenv("OUTLINE_CREDENTIALS"))
+    client = OutlineVPN(api_url=os.getenv("OUTLINE_CREDENTIALS"))   # pylint: disable=W0621
     yield client
 
 
-def test_get_keys(client: OutlineVPN):
+def test_get_keys(client: OutlineVPN):  # pylint: disable=W0621
     """Test for the get keys method"""
     assert len(client.get_keys()) >= 1
 
 
-def test_cud_key(client: OutlineVPN):
+def test_cud_key(client: OutlineVPN):  # pylint: disable=W0621
     """Test creating/updating the name/deleting a key"""
     new_key = client.create_key()
     assert new_key is not None
@@ -33,7 +33,7 @@ def test_cud_key(client: OutlineVPN):
     assert client.delete_key(new_key.key_id)
 
 
-def test_limits(client: OutlineVPN):
+def test_limits(client: OutlineVPN):  # pylint: disable=W0621
     """Test setting and removing limits"""
     assert client.add_data_limit(0, 1024 * 1024 * 20)
     assert client.delete_data_limit(0)
