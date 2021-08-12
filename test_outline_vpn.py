8c34102f43b4ff823c260442b53fa443d876c045
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2021-08-12 16:13:43 +0200

    Add module and test docstrings

--- test_outline_vpn.py
+++ test_outline_vpn.py
@@ -1,9 +1,14 @@
+"""
+Integration tests for the API wrapper
+"""
+
 import os
 
 from outline_vpn import OutlineVPN
 
 
 def test_get_keys():
+    """Test for the get keys method"""
     assert os.getenv("OUTLINE_CREDENTIALS")
     api_url = f'https://127.0.0.1:{os.getenv("OUTLINE_CREDENTIALS")}'
     print(api_url)
