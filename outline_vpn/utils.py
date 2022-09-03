10e9222b300ee6a68e9d7ec570df0c65c0ae29cb
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-09-04 00:40:39 +0200

    - Rename wrappedSocket to wrapped_socket - Separate exceptions into separate classes

--- outline_vpn/utils.py
+++ outline_vpn/utils.py
@@ -4,19 +4,27 @@ import hashlib
 from urllib.parse import urlparse
 
 
+class OutlineConnectionError(Exception):
+    pass
+
+
+class OutlineInvalidFingerprintError(Exception):
+    pass
+
+
 def check_ssl_fingerprint(api_url: str, cert_sha256: str) -> bool:
     url = urlparse(api_url)
     address = url.hostname
     port = url.port
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    wrappedSocket = ssl.wrap_socket(sock)
+    wrapped_socket = ssl.wrap_socket(sock)
     try:
-        wrappedSocket.connect((address, port))
+        wrapped_socket.connect((address, port))
     except Exception as e:
-        raise Exception(f"Connection Error: {e}")
+        raise OutlineConnectionError(f"Connection Error: {e}")
     else:
-        der_cert = wrappedSocket.getpeercert(True)
+        der_cert = wrapped_socket.getpeercert(True)
         thumb_sha256 = hashlib.sha256(der_cert).hexdigest()
         if thumb_sha256.upper() != cert_sha256.upper():
-            raise Exception("Invalid fingerprint!")
+            raise OutlineInvalidFingerprintError("Invalid fingerprint!")
     return True
