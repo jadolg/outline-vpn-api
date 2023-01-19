70390e0d04c3f1b2ea5c1550857c682643f46a83
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2023-01-19 12:08:14 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="outline-vpn-api",
-    version="2.1.0",
+    version="2.3.0",
     packages=["outline_vpn"],
     url="https://github.com/jadolg/outline-vpn-api/",
     license="MIT",
